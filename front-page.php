125e924fd4c7c34ed9093a74fb231b20e9cc7e2f
Baptiste Wllme <baptiste.willaume.pro@gmail.com>
2025-10-24 17:05:54 +0200

    initialisation portfolio

---
+++ front-page.php
@@ -0,0 +1,37 @@
+<?php get_header();
+?>
+
+
+<main class="hero">
+    <div class="hero_space"></div>
+    <h1 class="hero_title">Baptiste Willaume</h1>
+    <p class="hero_subtitle">Développeur wordpress</p>
+    <div class="hero_links">
+        <a href="https://www.linkedin.com/in/baptiste-willaume-122493191/" target="_blank">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/linkedin.svg" alt="linkedin">
+        </a>
+        <a href="">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mail.svg" alt="mail">
+        </a>
+        <a href="">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/phone.svg" alt="phone">  
+        </a>
+        <a href="https://github.com/Baptiste-Wllme" target="_blank">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/github.svg" alt="github">
+        </a>
+        <a href="">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/download.svg" alt="download">
+        </a> 
+    </div>
+    <div class="hero_scroll">
+        <p class="text">scroll</p>
+        <a href="#">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/flèche_01.svg" alt="arrow scroll">
+        </a>
+    </div>
+</main>
+
+
+<?php
+get_footer(); 
+?>
---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) exit;
+
+function blankslate_child_enqueue_styles() {
+    
+    wp_enqueue_style(
+        'blankslate-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('blankslate-parent-style'),
+        filemtime(get_stylesheet_directory() . '/style.css')
+    );
+}
+add_action('wp_enqueue_scripts', 'blankslate_child_enqueue_styles');
